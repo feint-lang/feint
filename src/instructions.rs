cc4184da89ed96d8a3a6027e81343eae3051fbc5
Wyatt Baldwin <self@wyattbaldwin.com>
2021-05-05 21:56:50 -0700

    WIP

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,9 +1,9 @@
 #[derive(Clone, Debug)]
 pub enum Instruction {
-    Print(u8),       // Print value(s) at top of stack
-    Constant(usize), // ???
-    Jump(usize),     // Jump uncoditionally
-    JumpIf(usize),   // Jump if top of stack is true
+    Print(u8),        // Print value(s) at top of stack
+    PushConst(usize), // ???
+    Jump(usize),      // Jump uncoditionally
+    JumpIf(usize),    // Jump if top of stack is true
     Return(usize),
     Push(usize),
     Add, // Add top items in stack; push result back
